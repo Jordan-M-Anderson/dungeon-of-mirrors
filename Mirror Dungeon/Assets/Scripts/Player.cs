ff18b1d4f0e50dc9e1d830b9dfe7b4e70bdb68a5
Jordan-M-Anderson <47698298+Jordan-M-Anderson@users.noreply.github.com>
2020-10-21 17:56:39 -0500

    Added animations and movement
    I had to add some animations to the player so you could tell where they were facing.

---
+++ Mirror Dungeon/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb2d;
+    public Animator animator;
+    Vector2 movement;
+
+    void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+
+}
