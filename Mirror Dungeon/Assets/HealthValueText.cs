28f19be6606dd1bf3b9445313a13eddce7dd30a4
Johncassetta <johncassetta@letu.edu>
2020-11-17 17:07:03 -0600

    Health bar update
    Health bar should now always be in the bottom-left corner of the screen, and there is a number beside the health bar showing th player's current health numerically.

---
+++ Mirror Dungeon/Assets/HealthValueText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthValueText : MonoBehaviour
+{
+    public Slider slider;
+    public Text healthText;
+    
+    public void updateHealth()
+    {
+        healthText.text = slider.value.ToString() + "/3";
+    }
+}
